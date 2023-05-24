c00f51ee0647eae89e6ccc2748cd6c6ea9d33d6f
Benedict <benedictryan80@gmail.com>
2023-05-24 15:43:45 +0700

    simplify final implementation

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Projects from './scenes/Projects';
 import Contact from './scenes/Contact';
 import Footer from './scenes/Footer';
 import useMediaQuery from './hooks/useMediaQuery';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, Fragment } from 'react';
 import Testimonials from './scenes/Testimonials';
 import { motion } from 'framer-motion';
 
@@ -16,6 +16,30 @@ function App() {
 	const [isTopOfPage, setIsTopOfPage] = useState(true);
 	const isDesktop = useMediaQuery('(min-width: 1060px)');
 
+	const navs = [
+		{
+			currentPage: 'home',
+			component: <Landing setSelectedPage={setSelectedPage} />
+		},
+		{
+			currentPage: 'skills',
+			component: <MySkills />
+		},
+		{
+			currentPage: 'projects',
+			component: <Projects />
+		},
+		{
+			currentPage: 'testimonials',
+			component: <Testimonials />
+		},
+		{
+			currentPage: 'contact',
+			component: <Contact />
+		}
+	];
+
+	// * NavBar will turn red if it's no longer at the top of page
 	useEffect(() => {
 		const handleScroll = () => {
 			if (window.scrollY === 0) {
@@ -35,61 +59,30 @@ function App() {
 				selectedPage={selectedPage}
 				setSelectedPage={setSelectedPage}
 			/>
-			{/* <div className="w-5/6 mx-auto md:h-full">
-				{isDesktop && (
-					<DotGroup
-						selectedPage={selectedPage}
-						setSelectedPage={setSelectedPage}
-					/>
-				)}
-				<motion.div
-					margin="0 0 -200px 0"
-					amount="all"
-					onViewportEnter={() => setSelectedPage('home')}
-				>
-					<Landing setSelectedPage={setSelectedPage} />
-				</motion.div>
-			</div> */}
-			<LineGradient />
-			{/* <div className="w-5/6 mx-auto md:h-full ">
-				<motion.div
-					margin="0 0 -200px 0"
-					amount="all"
-					onViewportEnter={() => setSelectedPage('skills')}
-				>
-					<MySkills />
-				</motion.div>
-			</div> */}
-			<LineGradient />
-			{/* <div className="w-5/6 mx-auto">
-				<motion.div
-					margin="0 0 -200px 0"
-					amount="all"
-					onViewportEnter={() => setSelectedPage('projects')}
-				>
-					<Projects />
-				</motion.div>
-			</div> */}
-			<LineGradient />
-			{/* <div className="w-5/6 mx-auto md:h-full">
-				<motion.div
-					margin="0 0 -200px 0"
-					amount="all"
-					onViewportEnter={() => setSelectedPage('testimonials')}
-				>
-					<Testimonials />
-				</motion.div>
-			</div> */}
-			<LineGradient />
-			{/* <div className="w-5/6 mx-auto md:h-full">
-				<motion.div
-					margin="0 0 -200px 0"
-					amount="all"
-					onViewportEnter={() => setSelectedPage('contact')}
-				>
-					<Contact />
-				</motion.div>
-			</div> */}
+			{isDesktop && (
+				<DotGroup
+					selectedPage={selectedPage}
+					setSelectedPage={setSelectedPage}
+				/>
+			)}
+			{navs.map(({ currentPage, component }, idx) => (
+				<Fragment key={idx}>
+					<div
+						className={`w-5/6 mx-auto ${
+							currentPage !== 'projects' && 'md:h-full'
+						}`}
+					>
+						<motion.div
+							margin="0 0 -200px 0"
+							amount="all"
+							onViewportEnter={() => setSelectedPage(currentPage)}
+						>
+							{component}
+						</motion.div>
+					</div>
+					<LineGradient />
+				</Fragment>
+			))}
 			<Footer />
 		</div>
 	);
--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -2,7 +2,7 @@ import SocialMediaIcons from '../components/SocialMediaIcons';
 import useMediaQuery from '../hooks/useMediaQuery';
 import { motion } from 'framer-motion';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
-
+// * Good for Portfolio when showcasing your Profile
 const Landing = ({ setSelectedPage }) => {
 	const isAboveLarge = useMediaQuery('(min-width: 1060px)');
 	return (
